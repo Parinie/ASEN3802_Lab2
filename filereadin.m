0b9fd04876ce94565cc1cad99816ed84cc006680
Parinie <pariniegupta.pg@gmail.com>
2025-10-02 18:50:49 -0600

    changes

--- filereadin.m
+++ filereadin.m
@@ -38,19 +38,22 @@ for i=1:5
     if (b{1} == "Aluminum") && (b{2} == "25V") && (b{3} == "240mA")
         %temp from thermocouples at t=0
         temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
-        length = linspace(0,0.2159,length(temp_time0));
+        l_time0 = linspace(0,0.1905,length(temp_time0));
 
         %fitting
-        p = polyfit(length,temp_time0,1);
-        y1 = polyval(p,length);
+        p1 = polyfit(l_time0,temp_time0,1);
+        y1 = polyval(p1,l_time0);
 
         %plot
         figure;
+        grid("on")
         hold on;
-        plot(length,y1)
+        plot(l_time0,y1,LineWidth=2)
+        ylim([16.6,16.7]);
         xlabel("Length along rod (m)")
         ylabel("Temp (deg C)")
         title('Aluminum 25V 240mA')
+        subtitle('Initial State')
 
         %extract T0
         T0 = y1(1); %deg C
@@ -59,99 +62,183 @@ for i=1:5
         %H_an = VI/kA
         H_an = (volts(i) * amps(i)) / (k(1) * cross_sec);
 
-        %calculating H_exp
         %steady state temp
-        % steady_temp = [data(end,2),data(end,2),data(end,3),data(end,4),data(end,5),data(end,6),data(end,7),data(end,8)];
-        % p = polyfit(length,steady_temp,1);
-        % y1 = polyval(p,length);
-        % figure;
-        % plot(length,y1)
+        steady_temp = [data(end,2),data(end,2),data(end,3),data(end,4),data(end,5),data(end,6),data(end,7),data(end,8)];
+        l_steady = linspace(0,0.1905,length(steady_temp));
+        p2 = polyfit(l_steady,steady_temp,1);
+        y2 = polyval(p2,l_steady);
+        figure;
+        hold on;
+        grid("on")
+        plot(l_steady,y2,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Aluminum 25V 240mA')
+        subtitle('Steady State')
 
-        
+        %calculating H_exp
 
-    % elseif (b{1} == "Aluminum") && (b{2} == "30V") && (b{3} == "290mA")
-    %     %temp from thermocouples at t=0
-    %     temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
-    %     length = linspace(0,0.2159,length(temp_time0));
-    % 
-    %     %fitting
-    %     p = polyfit(length,temp_time0,1);
-    %     y1 = polyval(p,length);
-    % 
-    %     %extract T0
-    %     T0 = y1(1); %deg C
-    % 
-    %     %plot
-    %     figure;
-    %     hold on;
-    %     plot(length,y1)
-    %     xlabel("Length along rod (m)")
-    %     ylabel("Temp (deg C)")
-    %     title('Aluminum 30V 290mA')
-    % 
-    %     %calculating H_an
-    % elseif (b{1} == "Brass") && (b{2} == "30V") && (b{3} == "285mA")
-    %     %temp from thermocouples at t=0
-    %     temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
-    %     length = linspace(0,0.2159,length(temp_time0));
-    % 
-    % 
-    %     %fitting
-    %     p = polyfit(length,temp_time0,1);
-    %     y1 = polyval(p,length);
-    % 
-    %     %extract T0
-    %     T0 = y1(1); %deg C
-    % 
-    %     %plot
-    %     figure;
-    %     hold on;
-    %     plot(length,y1)
-    %     xlabel("Length along rod (m)")
-    %     ylabel("Temp (deg C)")
-    %     title('Brass 25V 237mA')
+    elseif (b{1} == "Aluminum") && (b{2} == "30V") && (b{3} == "290mA")
+    %temp from thermocouples at t=0
+        temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
+        l_time0 = linspace(0,0.1905,length(temp_time0));
+
+        %fitting
+        p1 = polyfit(l_time0,temp_time0,1);
+        y1 = polyval(p1,l_time0);
+
+        %plot
+        figure;
+        grid("on")
+        hold on;
+        plot(l_time0,y1,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Aluminum 30V 290mA')
+        subtitle('Initial State')
+
+        %extract T0
+        T0 = y1(1); %deg C
 
         %calculating H_an
-    % elseif (b{1} == "Brass") && (b{2} == "25V") && (b{3} == "237mA")
-    %     %temp from thermocouples at t=0
-    %     temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
-    % 
-    %     %fitting
-    %     p = polyfit(length,temp_time0,2);
-    %     y1 = polyval(p,length);
-    % 
-    %     %extract T0
-    %     T0 = y1(1); %deg C
-    % 
-    %     %plot
-    %     figure;
-    %     hold on;
-    %     plot(length,y1)
-    %     xlabel("Length along rod (m)")
-    %     ylabel("Temp (deg C)")
-    % 
-    %     %calculating H_an
-    % elseif (b{1} == "Steel") && (b{2} == "22V") && (b{3} == "203mA")
-    %     %temp from thermocouples at t=0
-    %     temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
-    % 
-    %     %fitting
-    %     p = polyfit(length,temp_time0,2);
-    %     y1 = polyval(p,length);
-    % 
-    %     %extract T0
-    %     T0 = y1(1); %deg C
-    % 
-    %     %plot
-    %     figure;
-    %     hold on;
-    %     plot(length,y1)
-    %     xlabel("Length along rod (m)")
-    %     ylabel("Temp (deg C)")
-    % 
-    %     %calculating H_an
-    % else
-    %     disp("Error: Incorrect material properties")
+        %H_an = VI/kA
+        H_an = (volts(i) * amps(i)) / (k(1) * cross_sec);
+
+        %steady state temp
+        steady_temp = [data(end,2),data(end,2),data(end,3),data(end,4),data(end,5),data(end,6),data(end,7),data(end,8)];
+        l_steady = linspace(0,0.1905,length(steady_temp));
+        p2 = polyfit(l_steady,steady_temp,1);
+        y2 = polyval(p2,l_steady);
+        figure;
+        hold on;
+        grid("on")
+        plot(l_steady,y2,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Aluminum 30V 290mA')
+        subtitle('Steady State')
+
+    elseif (b{1} == "Brass") && (b{2} == "25V") && (b{3} == "237mA")
+    %temp from thermocouples at t=0
+        temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
+        l_time0 = linspace(0,0.1905,length(temp_time0));
+
+        %fitting
+        p1 = polyfit(l_time0,temp_time0,1);
+        y1 = polyval(p1,l_time0);
+
+        %plot
+        figure;
+        grid("on")
+        hold on;
+        plot(l_time0,y1,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Brass 25V 237mA')
+        subtitle('Initial State')
+
+        %extract T0
+        T0 = y1(1); %deg C
+
+        %calculating H_an
+        %H_an = VI/kA
+        H_an = (volts(i) * amps(i)) / (k(2) * cross_sec);
+
+        %steady state temp
+        steady_temp = [data(end,2),data(end,2),data(end,3),data(end,4),data(end,5),data(end,6),data(end,7),data(end,8)];
+        l_steady = linspace(0,0.1905,length(steady_temp));
+        p2 = polyfit(l_steady,steady_temp,1);
+        y2 = polyval(p2,l_steady);
+        figure;
+        hold on;
+        grid("on")
+        plot(l_steady,y2,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Brass 25V 237mA')
+        subtitle('Steady State')
+
+    elseif (b{1} == "Brass") && (b{2} == "30V") && (b{3} == "285mA")
+    %temp from thermocouples at t=0
+        temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
+        l_time0 = linspace(0,0.1905,length(temp_time0));
+
+        %fitting
+        p1 = polyfit(l_time0,temp_time0,1);
+        y1 = polyval(p1,l_time0);
+
+        %plot
+        figure;
+        grid("on")
+        hold on;
+        plot(l_time0,y1,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Brass 30V 285mA')
+        subtitle('Initial State')
+
+        %extract T0
+        T0 = y1(1); %deg C
+
+        %calculating H_an
+        %H_an = VI/kA
+        H_an = (volts(i) * amps(i)) / (k(2) * cross_sec);
+
+        %steady state temp
+        steady_temp = [data(end,2),data(end,2),data(end,3),data(end,4),data(end,5),data(end,6),data(end,7),data(end,8)];
+        l_steady = linspace(0,0.1905,length(steady_temp));
+        p2 = polyfit(l_steady,steady_temp,1);
+        y2 = polyval(p2,l_steady);
+        figure;
+        hold on;
+        grid("on")
+        plot(l_steady,y2,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Brass 30V 285mA')
+        subtitle('Steady State')
+
+    elseif (b{1} == "Steel") && (b{2} == "22V") && (b{3} == "203mA")
+    %temp from thermocouples at t=0
+        temp_time0 = [data(1,2),data(1,2),data(1,3),data(1,4),data(1,5),data(1,6),data(1,7),data(1,8),data(1,9)];
+        l_time0 = linspace(0,0.1905,length(temp_time0));
+
+        %fitting
+        p1 = polyfit(l_time0,temp_time0,1);
+        y1 = polyval(p1,l_time0);
+
+        %plot
+        figure;
+        grid("on")
+        hold on;
+        plot(l_time0,y1,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Steel 22V 230mA')
+        subtitle('Initial State')
+
+        %extract T0
+        T0 = y1(1); %deg C
+
+        %calculating H_an
+        %H_an = VI/kA
+        H_an = (volts(i) * amps(i)) / (k(3) * cross_sec);
+
+        %steady state temp
+        steady_temp = [data(end,2),data(end,2),data(end,3),data(end,4),data(end,5),data(end,6),data(end,7),data(end,8)];
+        l_steady = linspace(0,0.1905,length(steady_temp));
+        p2 = polyfit(l_steady,steady_temp,1);
+        y2 = polyval(p2,l_steady);
+        figure;
+        hold on;
+        grid("on")
+        plot(l_steady,y2,LineWidth=2)
+        xlabel("Length along rod (m)")
+        ylabel("Temp (deg C)")
+        title('Steel 22V 230mA')
+        subtitle('Steady State')
+    else
+        disp("Error: Incorrect material properties")
     end
 
 end
